a94af9b6c0953add6a67431c02b5b6e2c0dc15de
AyushRaj-10 <ayushrajranjan10@gmail.com>
2025-05-25 15:14:42 +0530

    Update Founders.tsx

--- src/components/Founders.tsx
+++ src/components/Founders.tsx
@@ -6,7 +6,7 @@ export const founderData = [
   {
     name: "Ayush Raj",
     role: "Co-founder & CEO",
-    import AyushImg from './assets/Ayush.png.jpeg';
+    import AyushImg from './public/Ayush.png.jpeg';
     <img src={AyushImg} alt="Ayush" />,
     bio: "Ayush is an 18-year-old Computer Science student at Dayananda Sagar College of Engineering. He founded GreenRoutine as a college project to make sustainable living accessible and engaging for everyone.",
     gender: "male"
